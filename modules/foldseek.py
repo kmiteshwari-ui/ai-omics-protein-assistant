d47e33466460c8b85806f7010009d336dd02f44a
kmiteshwari-ui <kmiteshwari@gmail.com>
2026-08-19 11:53:59 +0530

    Add search_foldseek function to query Foldseek API

--- modules/foldseek.py
+++ modules/foldseek.py
@@ -1,1 +1,22 @@
+import requests
 
+
+def search_foldseek(protein_id):
+    url = "https://search.foldseek.com/api/search"
+
+    params = {
+        "query": protein_id,
+        "database[]": "pdb100",
+        "mode": "3diaa"
+    }
+
+    try:
+        response = requests.get(url, params=params, timeout=30)
+
+        if response.status_code != 200:
+            return None
+
+        return response.json()
+
+    except requests.RequestException:
+        return None
